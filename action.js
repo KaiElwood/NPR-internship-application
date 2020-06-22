eca519010df225ea63977af06a1a59cb8b9f61d8
KED <kaipie3@gmail.com>
2020-06-22 17:26:21 -0400

    updated js

--- action.js
+++ action.js
@@ -1,7 +1,13 @@
 const book = document.getElementById("book")
 const findOut = document.getElementById("element")
+const goBack = document.getElementById("goBack")
 
 findOut.addEventListener("click", function(){
+    book.classList.toggle("poems")
+    book.classList.toggle("moveRight")
+})
+
+goBack.addEventListener("click", function () {
     book.classList.toggle("poems")
     book.classList.toggle("moveRight")
 })
